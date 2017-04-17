e255af3c6009703ae69dfd706e61466db915bdb7
Siobhan <siobhan.harley@gmail.com>
2017-04-17 14:11:05 -0400

    configure omniauth and figaro

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :pin_endorsements
   resources :comments
   resources :boards
+  get '/auth/facebook/callback' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
