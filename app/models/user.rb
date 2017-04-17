ef83496d409bd53b94c2823520a8e13368eaf48e
Siobhan <siobhan.harley@gmail.com>
2017-04-17 15:36:50 -0400

    add sessions helper method for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-  has_secure_password 
+  has_secure_password
+
+ validates :username, presence: true, length: { maximum: 15 }
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ validates :email, presence: true,
+                   format: { with: VALID_EMAIL_REGEX },
+                   uniqueness: { case_sensitive: false }
+ validates :password, presence: true, length: { minimum: 6 }
+
 end
