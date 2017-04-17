dd52ada3ef6c6ab8a69ac2155a59dd142581cd8d
Siobhan <siobhan.harley@gmail.com>
2017-04-17 16:59:18 -0400

    stubbed out pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,20 @@
 class PinsController < ApplicationController
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+  end
+
+  def show
+    @pin = Pin.find_by(id: params[:id])
+  end 
+
+  private
+
+  def pin_params
+  end
+
 end
