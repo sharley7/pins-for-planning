d5ff2c37567bb95f83e013387a51e39a59157933
Siobhan <siobhan.harley@gmail.com>
2017-04-17 20:17:13 -0400

    comment feature on show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+    redirect_to pin_path(@comment.pin)
+  end
+
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :pin_id, user_attributes: [:username, :password, :email])
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def logged_in?
+    !!current_user 
+  end
+
 end
