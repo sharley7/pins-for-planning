7e7f09e92f29bc53ce24e5d5c5809a13c79bb071
Siobhan <siobhan.harley@gmail.com>
2017-04-21 20:18:19 -0400

    fixed blank tag issue in pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -8,7 +8,7 @@ class Pin < ApplicationRecord
   validates :image_file_name, presence: true
   validates :description, length: { in: 10..200 }
 
-  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['title'].blank? }
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
 
   has_attached_file :image, :styles => { :medium => "300x300#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
