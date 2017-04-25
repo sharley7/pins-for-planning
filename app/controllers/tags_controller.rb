2e02defaf8b747eea4e83c4b8caa28d3adfcfab1
Siobhan <siobhan.harley@gmail.com>
2017-04-25 00:04:22 -0400

    404 page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+  load_and_authorize_resource
 
   def index
     @tags = Tag.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  load_and_authorize_resource
   def new
    if !logged_in?
     @user = User.new
