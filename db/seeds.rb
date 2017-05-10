315d0f9ddaa5532312a6fefa09bef30c84850f76
Siobhan <siobhan.harley@gmail.com>
2017-05-10 12:34:32 -0400

    removed pins from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,4 @@
-Pin.create(title: "Eliminate Columbus Park Fencing", description: "The fencing should be removed and a pathway should be created to allow people to walk through the park. Right now, it is not integrated with the streetscape.", user_id: 1, image_file_name: "Screen_Shot_2017-04-20_at_1.28.12_PM.png", image_content_type: "image/png", image_file_size: 719821, image_updated_at: "2017-04-20 17:32:28")
 
-  Pin.create(title: "A new Love Letters to Brooklyn", description: "Absolutely love the Stephen Powers Love Letters to Brooklyn piece and was so sad to hear it will be demolished. It would be wonderful if we could recreate this somewhere else.", user_id: 2, image_file_name: "Screen_Shot_2017-04-20_at_1.38.10_PM.png", image_content_type: "image/png", image_file_size: 1661875, image_updated_at: "2017-04-20 17:49:33")
-
- Pin.create(title: "More innovative design solutions”, description: "I appreciate how the security kiosks enhance the streetscape, instead of detract for it. We need more design solutions like this.", user_id: 3,  created_at: "2017-04-20 18:13:27",  image_file_name: "Screen_Shot_2017-04-20_at_2.10.05_PM.png", image_content_type: "image/png", image_file_size: 2235735, image_updated_at: "2017-04-20 18:13:27")
 
 
 User.create(username: "BrooklynDad77", password: "catsaresupercool", email: "brooklyndad@aol.com")
